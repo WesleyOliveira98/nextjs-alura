89201223841e7e41257354708347a41f318b42b7
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-03-20 16:25:20 -0300

    Criação da pasta public com o arquivo robots e imagens estaticas; Abstração da tela home para componente na pasta src; Criação do componente de Footer; Adicionando layout personalizado na pasta theme

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,3 @@
-import Link from "../src/components/Link";
+import HomeScreen from '../src/screens/HomeScreen';
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <>
-            <Tag>{children}</Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
-    )
-}
-
-export default function Home() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases - Home Page</Title>
-            <Link href="/faq">
-                Ir para FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
