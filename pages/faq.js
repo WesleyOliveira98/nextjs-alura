0a0378b3c03cd83d0323d82004f35d000dc1ea0c
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-03-20 16:59:46 -0300

    Adicionando consultas na API para encontrar as perguntas e respostas da FAQ; FAzendo a requisição antes da renderização da página usando a função getStaticProps do Next.js

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,36 @@
+import * as React from 'react';
 import Link from "../src/components/Link";
 
-export default function FAQ() {
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+            .then(response => { 
+                return response.json(); 
+            })
+    return {
+        props: {
+            faq
+        }
+    };
+}
+
+export default function FAQ({ faq }) {
     return (
         <div>
             <h1>Alura Cases - FAQ</h1>
             <Link href="/">
                 Ir para Home
             </Link>
+            <ul>
+                {faq.map(item => {
+                    return (
+                        <article key={item.question}>
+                            <h2>{item.question}</h2>
+                            <p>{item.answer}</p>
+                        </article>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
