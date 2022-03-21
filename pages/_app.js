388eebc567c3cc3b10d5fdd9a439899ae3c970e2
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-03-20 22:50:56 -0300

    -

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function MyApp({ Component, pageProps }) {
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-ETC9CQ8PRW"></script>
                 <script 
                     dangerouslySetInnerHTML={{
-                        __hrml: `
+                        __html: `
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
@@ -19,7 +19,7 @@ export default function MyApp({ Component, pageProps }) {
                     }}
                 />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
             </Head>
             <GlobalStyle />
